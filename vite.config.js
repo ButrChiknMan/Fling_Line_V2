799727c7d7a437395d3c6320cab2abba30f32db6
ButrChiknMan <sidaksinghkalsi@gmail.com>
2026-07-26 19:41:48 +0900

    chnged root build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/my-new-project/',
+  base: '/Fling_Line_V2/',
   build: {
     outDir: 'docs',
     rollupOptions: {
