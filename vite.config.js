0c361d349bed3340b993a502d34c9f78eb0c1af5
ButrChiknMan <sidaksinghkalsi@gmail.com>
2026-07-26 19:19:13 +0900

    added rollup otptions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/my-new-project/',
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+      },
+    },
   },
 })
